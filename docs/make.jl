ab2b789a9f19141f68274eddb8ebf519c6575ec4
Sam Miller <smil@lle.rochester.edu>
2022-09-08 14:11:58 -0400

    Basic docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,31 @@
-using Documenter
-using BlockHaloArrays
+using Documenter, BlockHaloArrays
+
+DocMeta.setdocmeta!(BlockHaloArrays, :DocTestSetup, :(using BlockHaloArrays); recursive=true)
 
 makedocs(
-    sitename = "BlockHaloArrays",
-    format = Documenter.HTML(),
-    modules = [BlockHaloArrays]
+    source = "src",
+    build = "build",
+    clean = true,
+    doctest = true,
+    repo = "https://github.com/smillerc/BlockHaloArrays.jl",
+    highlightsig = true,
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+    expandfirst = [],
+    modules = [BlockHaloArrays],
+    pages = [
+        "BlockHaloArrays.jl" => "index.md",
+        # "Examples" => example_md_files,
+        "Reference" => [
+            # "MPIHaloArray" => "mpihaloarray.md",
+            # "Topology" => "topology.md",
+            "API" => "api.md"
+        ]
+    ],
+    sitename = "BlockHaloArrays.jl"
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(repo = "github.com/smillerc/BlockHaloArrays.jl.git",
+           branch = "gh-pages",
+           devbranch = "main",)
